b131764cdea817dce5f897c282e2dde02d01a98d
Badger-Jay <jy3287@columbia.edu>
2025-01-08 22:23:46 -0500

    Initial commit

--- SwiftCodePathApp/ViewController.swift
+++ SwiftCodePathApp/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  SwiftCodePathApp
+//  CodePathSwift
 //
 //  Created by Alex Yu on 1/8/25.
 //
@@ -8,7 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func changeBackgroundColor(_ sender: Any) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
 
+    func changeColor() -> UIColor{
+
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
